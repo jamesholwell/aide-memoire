7403c462982b717b000cba0f39da7761590251c0
James Holwell <3832591+jamesholwell@users.noreply.github.com>
2025-06-13 21:20:02 +0100

    refactor commands to use handler pattern
    * handlers are discovered by reflection
    * console is passed to handler

---
+++ AideMemoire/Handlers/AboutHandler.cs
@@ -0,0 +1,18 @@
+using System.CommandLine;
+
+namespace AideMemoire.Handlers;
+
+public class AboutHandler : IApplicationHandler {
+    public void RegisterCommand(RootCommand root) {
+        var aboutCommand = new Command("about", "Show information about the application");
+        aboutCommand.SetHandler(ctx => ShowAboutInformation(ctx.Console));
+
+        root.AddCommand(aboutCommand);
+    }
+
+    internal static void ShowAboutInformation(IConsole console) {
+        console.WriteLine("aide-mémoire: n. a thing, especially a book or document, that helps you to remember something");
+        console.WriteLine("v0.1");
+        console.WriteLine(string.Empty);
+    }
+}
